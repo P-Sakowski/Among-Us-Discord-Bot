e32bf7ef884f459a5d9747e6b010bc221c069da7
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-11-12 17:37:58 +0100

    main library changed from discord.io to discord.js

--- bot.js
+++ bot.js
@@ -1,50 +1,37 @@
-const Discord = require('discord.io');
-const logger = require('winston');
+const Discord = require("discord.js");
 const auth = require('./auth.json');
+const client = new Discord.Client();
 
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+client.on("ready", () => {
+  console.log("I am ready!");
 });
-logger.level = 'debug';
-const bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on("message", (message) => {
+  if (message.content.startsWith(".ping")) {
+    message.channel.send("Pong!");
+  }
+  if (message.content.startsWith(".mute")) {
+    if (message.member.voice.channel) {
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+        for (const [memberID, member] of channel.members) {
+          member.voice.setMute(true);
+        }
+        message.channel.send("Muted!");
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+}
+  if (message.content.startsWith(".unmute")) {
+    if (message.member.voice.channel) {
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+        for (const [memberID, member] of channel.members) {
+          member.voice.setMute(false);
+        }
+        message.channel.send("Unmuted!");
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // It will listen for messages that will start with `.`
-    if (message.substring(0, 1) == '.') {
-        let args = message.substring(1).split(' ');
-        const cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            /*case 'mute':
-                if (message.member.voice.channel) {
-                    let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-                    for (const [memberID, member] of channel.members) {
-                      // if (member != message.member)
-                      member.voice.setMute(true);
-                    }
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Muted!'
-                    });
-                } else {
-                    message.reply('You need to join a voice channel first!');
-                }
-            break; */
-         }
-     }
-});
+
+client.login(auth.token);
